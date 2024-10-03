7e162d276e557064f1665e6aee53395fe35fcaf1
minhhoang33 <hoanggok@gmail.com>
2024-10-04 01:23:42 +0700

    Add files via upload

---
+++ bai-tap-2/src/Main.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ten cua ban: ");
+        String name = sc.nextLine();
+        System.out.println("Nhap tuoi cua ban: ");
+        int age = Integer.parseInt(sc.nextLine());
+        String ageWarning = (age < 0 || age > 100) ? "Canh bao: Tuoi khong hop le! Vui long nhap lai." : "";
+        System.out.println(ageWarning);
+        System.out.println("Nhap mon hoc cua ban: ");
+        String subject = sc.nextLine();
+        System.out.println("Nhap email cua ban: ");
+        String email = sc.nextLine();
+        String emailWarning = (!email.contains("@")) ? "Cảnh báo: Email không hợp lệ! Vui lòng nhập lại." : "";
+        System.out.println(emailWarning);
+        System.out.println("Nhap dia chi cua ban: ");
+        String diaChi = sc.nextLine();
+    }
+}
